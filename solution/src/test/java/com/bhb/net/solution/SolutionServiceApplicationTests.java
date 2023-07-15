ef0bac5df8888ca5bd6d0db6bbe6db2710219e35
Bereket H <bhbbereket@gmail.com>
2023-07-15 19:53:10 -0400

    third and final initial commit

---
+++ solution/src/test/java/com/bhb/net/solution/SolutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhb.net.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
