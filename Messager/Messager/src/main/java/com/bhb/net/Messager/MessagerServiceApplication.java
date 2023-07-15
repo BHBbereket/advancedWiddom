696ac57ab4c6322d1ae693356f8218b5c0cb021b
Bereket H <bhbbereket@gmail.com>
2023-07-15 17:59:37 -0400

    initial commit

---
+++ Messager/Messager/src/main/java/com/bhb/net/Messager/MessagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bhb.net.Messager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagerServiceApplication.class, args);
+	}
+
+}
---
+++ Messager/Messager/src/test/java/com/bhb/net/Messager/MessagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhb.net.Messager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
